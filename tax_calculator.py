aa330c3b5cc3827bfd77bcce8f987fb2d1c51505
ajzett <36409591+ajzett@users.noreply.github.com>
2018-05-25 10:42:32 -0400

    Adjust black_suit_buff

--- tax_calculator.py
+++ tax_calculator.py
@@ -14,7 +14,7 @@ spray_can = G.item('Pepper Spray', 'A can of pepper spray.', 10)
 
 # Buffs/Heals #
 # Buffs
-black_suit_buff = G.stat_item('Mysterious Orange Liquid', 'A mysterious orange liquid that was used by the Man in the Black Suit.', 1000, 10, 0, 25, 5, -10)
+black_suit_buff = G.stat_item('Mysterious Orange Liquid', 'A mysterious orange liquid that was used by the Man in the Black Suit.', 1000, 10, 0, 25, 20, -10)
 msg = G.stat_item('MSG', 'Super Salt. Seriously bad for you, but a seriously wild ride.', 5, 2, 0, 10, 0, 10)
 tso_chicken = G.stat_item("General Tso's Chicken", "Nothing incites a fighting spirit like the effigy of General Tso's Chicken. Why did the General only have one?", 20, 3, 30)
 
